0173c48f430ce1ec26d63a5aae7a30fcaf0b8f9c
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-07-30 10:17:04 +1000

    Move single source apps into hash. App meta packages into meta.rb.

--- apps.rb
+++ apps.rb
@@ -1,50 +1,22 @@
-dep 'Applications' do
-  requires 'desktop_help_utilities',
-	   'Google Chrome.app',
-	   'Viscosity.app',
-           'RubyMine.app',
-           'Dropbox.app',
-	   'iTerm.app'
-end
+SIMPLE_APPS = {
+  'iTerm.app'  =>  'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip',
+  'Dropbox.app'  =>  'https://www.dropbox.com/download?plat=mac',
+  'SizeUp.app'  =>  'http://www.irradiatedsoftware.com/download/SizeUp.zip',
+  'Google Chrome.app' =>'https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg',
+  'Viscosity.app' =>'http://www.sparklabs.com/downloads/Viscosity.dmg',
+  'Flux.app' =>'https://justgetflux.com/mac/Flux.zip',
+  'RubyMine.app' =>'http://download.jetbrains.com/ruby/RubyMine-5.4.3.dmg',
+}
 
-dep 'iTerm.app' do
-  source 'http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip'
-end
-
-
-dep 'desktop_help_utilities' do
-  requires 'Flux.app',
-           'SizeUp.app'
-#	   'KeyRemap4MacBook.app'
-end
-
-
-dep 'Dropbox.app' do
-  source 'https://www.dropbox.com/download?plat=mac'
-end
-
-
-dep 'SizeUp.app' do
-  source 'http://www.irradiatedsoftware.com/download/SizeUp.zip'
+SIMPLE_APPS.each do |app_name, source_location|
+  dep app_name do
+    source source_location
+  end
 end
 
 dep 'KeyRemap4MacBook.app' do
 #  requires 'osx.GateKeeperDisabled'
   source 'https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.3.0.dmg'
-end
-
-
-dep 'Google Chrome.app' do
-  source 'https://dl-ssl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
-end
-dep 'Viscosity.app' do
-  source 'http://www.sparklabs.com/downloads/Viscosity.dmg'
-end
-dep 'Flux.app' do
-  source 'https://justgetflux.com/mac/Flux.zip'
-end
-dep 'RubyMine.app' do
-  source 'http://download.jetbrains.com/ruby/RubyMine-5.4.3.dmg'
 end
 
 dep 'Steam.app' do
--- meta.rb
+++ meta.rb
@@ -2,3 +2,17 @@ dep 'garrow' do
   requires 'terminal', 'Applications'
 end
 
+dep 'Applications' do
+  requires 'desktop_help_utilities',
+	   'Google Chrome.app',
+	   'Viscosity.app',
+           'RubyMine.app',
+           'Dropbox.app',
+	   'iTerm.app'
+end
+
+dep 'desktop_help_utilities' do
+  requires 'Flux.app',
+           'SizeUp.app'
+#	   'KeyRemap4MacBook.app'
+end
