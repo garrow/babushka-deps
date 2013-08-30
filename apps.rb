a7a18685b930a85eb97031a407ff8f8626439986
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-08-30 14:52:26 +1000

    Add Shiori, Kindle, Firefox, TotalSpaces, Parallels.

--- apps.rb
+++ apps.rb
@@ -8,12 +8,24 @@ SIMPLE_APPS = {
     'RubyMine' => 'http://download.jetbrains.com/ruby/RubyMine-5.4.3.dmg',
     'KeePassX' => 'http://downloads.sourceforge.net/keepassx/KeePassX-0.4.3.dmg',
     'VLC' => 'http://get.videolan.org/vlc/2.0.7/macosx/vlc-2.0.7.dmg',
+    'Shiori' => 'http://aki-null.net/shiori/release/Shiori_1.0.1.zip',
+    'Kindle' => 'http://kindleformac.amazon.com/40499/KindleForMac.dmg',
+    'Firefox' => 'http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/23.0.1/mac/en-US/Firefox%2023.0.1.dmg',
+
 }
+
+dep "dotapps" do
+  requires SIMPLE_APPS.keys.collect {|a| "#{a}.app" }
+end
 
 SIMPLE_APPS.each do |app_name, source_location|
   dep "#{app_name}.app", :template => 'app' do
     source source_location
   end
+end
+
+dep 'TotalSpaces.installer' do
+  source 'http://downloads-1.binaryage.com/TotalSpaces-1.2.9.zip'
 end
 
 dep 'KeyRemap4MacBook.app' do
@@ -27,4 +39,6 @@ dep 'Steam.app' do
   }
   source 'http://media.steampowered.com/client/installer/steam.dmg'
 end
-
+dep "Parallels Desktop.installer" do
+    source  'http://dl.cleverbridge.com/329/mshi5o-15530-1F58E6B1-47875247/ParallelsDesktop-8.0.18608.898384.dmg'
+end
