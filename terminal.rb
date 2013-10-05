592e1a2aedfd914890469e4092968517edb660ed
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2013-10-05 15:19:13 +1000

    Add wget.

--- terminal.rb
+++ terminal.rb
@@ -37,7 +37,7 @@ dep 'bash.case_insensitive_completion' do
 end
 
 
-utilities = %w{ bash ack vim tree git-flow }
+utilities = %w{ bash ack vim tree git-flow wget }
 managed_utilities = utilities.collect {|u| "#{u}.managed" }
 managed_utilities.each { |u| dep u }
 dep 'managed_utilities' do
