84a0e01f247dd79f64209d6c8cf94c524bd12834
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-07-08 15:56:33 +1000

    Remove duplicated bash settings

--- terminal/terminal.rb
+++ terminal/terminal.rb
@@ -7,37 +7,6 @@ end
 
 dep 'bcat.gem'
 
-dep 'bash' do
-  requires 'bash.enabled'
-end
-
-dep 'bash.allowed shells' do
-  met? {
-    "/etc/shells".p.grep("/usr/local/bin/bash")
-  }
-  meet {
-    sudo 'echo "/usr/local/bin/bash\n" >> /etc/shells'
-  }
-end
-
-dep 'bash.enabled' do
-  requires 'bash.managed', 'bash.allowed shells'
-
-  meet {
-    sudo 'chsh /usr/local/bin/bash/ `whoami`'
-  }
-end
-
-dep 'bash.case_insensitive_completion' do
-  met? {
-    "/etc/inputrc".p.grep("set completion-ignore-case on")
-  }
-  meet {
-    sudo 'echo "set completion-ignore-case on\n" >> /etc/inputrc'
-  }
-end
-
-
 utilities = %w{ bash ack vim tree wget }
 managed_utilities = utilities.collect {|u| "#{u}.managed" }
 managed_utilities.each { |u| dep u }
