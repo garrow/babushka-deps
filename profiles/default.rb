d89588a9d76dd9d4e55be5c7a3152c3967020120
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-07-08 15:46:47 +1000

    Move meta dep to profiles/

---
+++ profiles/default.rb
@@ -0,0 +1,14 @@
+dep 'default' do
+  requires 'terminal',
+           'development',
+           'Flux.app',
+           'SizeUp.app',
+           'KeyRemap4MacBook.app',
+           'Google Chrome.app',
+           'RubyMine.app',
+           'Dropbox.app',
+           'iTerm.app',
+           'KeePassX.app',
+           'VLC.app'
+end
+
