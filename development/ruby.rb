558a6735df2702571931e3b7596e9643e3287147
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-10-13 17:01:47 +1100

    Use ruby-install for rubies.

--- development/ruby.rb
+++ development/ruby.rb
@@ -1,18 +1,38 @@
 dep 'ruby.development' do
   requires 'chruby.managed',
-	   'ruby-install.managed',
-	   'memcached.managed',
-	   'postgresql',
-	   'qt.managed',
-	   'phantomjs.managed'
+           'ruby-2.1.1'
+	   #'memcached.managed',
+           'postgresql',
+	   #'qt.managed',
+	   #'phantomjs.managed'
+end
+
+dep 'ruby-2.1.1' do
+  requires 'ruby installed'.with('ruby', '2.1.1')
+end
+
+dep 'ruby installed', :runtime, :version do
+  runtime.default('ruby')
+  version.default('2.1.1')
+
+  requires 'chruby.managed',
+           'ruby-install.managed'
+
+  def ruby_slug
+    "#{runtime}-#{version}"
+  end
+
+  met? { "~/.rubies/#{ruby_slug}".p.exist? }
+  meet {
+    log "Installing Ruby #{runtime} #{version}"
+    shell "ruby-install #{runtime} #{version}", :log => true
+  }
 end
 
 # Brew provided packages
 dep 'ruby-install.managed'
 dep 'phantomjs.managed'
 
-# TODO - handle the steps that homebrew doesnt setup as a meta package. e.g launch daemons.
-dep 'memcached.managed'
 
 dep 'chruby.managed' do
   met? {
@@ -20,10 +40,11 @@ dep 'chruby.managed' do
   }
 end
 
+# TODO - handle the steps that homebrew doesnt setup as a meta package. e.g launch daemons.
+dep 'memcached.managed'
+
 # TODO - Ensure that homebrew is on path.
 
 dep 'qt.managed' do
   provides 'qmake'
 end
-
-
