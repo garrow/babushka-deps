61a6f923ffe453830d65d8ef05df0636dbf6b690
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2014-10-20 14:05:04 +1100

    Split Apps from v2 profile

---
+++ apps.rb
@@ -0,0 +1,41 @@
+dep "apps" do
+  requires 'alfred.cask',
+           'diffmerge.cask',
+           'fluid.cask',
+           'flux.cask',
+           'google-chrome.cask',
+           'iterm2.cask',
+           'libreoffice.cask',
+           'macpass.cask',
+           'macpass.cask',
+           'macvim.cask',
+           'marked.cask',
+           'omnigraffle.cask',
+           'rubymine.cask',
+           'sizeup.cask',
+           'slack.cask',
+           'spotify.cask',
+           'sublime-text.cask',
+           'vlc.cask'
+
+end
+
+dep 'alfred.cask'
+dep 'diffmerge.cask'
+dep 'fluid.cask'
+dep 'flux.cask'
+dep 'google-chrome.cask'
+dep 'iterm2.cask'
+dep 'libreoffice.cask'
+dep 'macpass.cask'
+dep 'macpass.cask'
+dep 'macvim.cask'
+dep 'marked.cask'
+dep 'omnigraffle.cask'
+dep 'rubymine.cask'
+dep 'sizeup.cask'
+dep 'slack.cask'
+dep 'spotify.cask'
+dep 'sublime-text.cask'
+dep 'totalspaces2.cask'
+dep 'vlc.cask'
